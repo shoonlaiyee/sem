7eb2d7511ccb1e0591470ab9e1b98a1ede49f7eb
shoonlaiyee <40440094@live.napier.ac.uk>
2020-01-08 15:30:12 +0630

    docker app.java update

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,8 +9,10 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Connect to MongoDB
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        //MongoClient mongoClient = new MongoClient("localhost", 27000);
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -22,7 +24,6 @@ public class App
                 .append("result", new Document("CW", 95).append("EX", 85));
         // Add document to collection
         collection.insertOne(doc);
-
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
