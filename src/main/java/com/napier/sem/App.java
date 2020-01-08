7f16624f384bc3c9786016dd3aadb0484923dd83
shoonlaiyee <40440094@live.napier.ac.uk>
2020-01-08 14:13:28 +0630

    app.java-update

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,7 +16,7 @@ public class App
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Kevin Chalmers")
+        Document doc = new Document("name", "shoonlaiyee")
                 .append("class", "Software Engineering Methods")
                 .append("year", "2018/19")
                 .append("result", new Document("CW", 95).append("EX", 85));
